e50300a767b3da39369ad9e1ee5348d8e135b35f
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-21 08:04:10 +0530

    import reducer

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
-
+import reducer from "./store/Reducers/MovieBrowser";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
