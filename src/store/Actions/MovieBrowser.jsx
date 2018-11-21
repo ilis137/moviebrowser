eebd036851e808e417a11b9b0eef6abd8c258e46
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-21 08:02:18 +0530

    update action creators

--- src/store/Actions/MovieBrowser.jsx
+++ src/store/Actions/MovieBrowser.jsx
@@ -1,10 +1,23 @@
 import * as actionTypes from "./actions";
 import Axios from "axios";
 
-export const getTopMovies = topMovies => {
+export const getTopMoviesStart = () => {
+  return dispatch => {
+    Axios.get(
+      "https://api.themoviedb.org/3/movie/top_rated?api_key=f59105d2523e3fcebe1e4a83f7abb717&language=en-US&page=1"
+    )
+      .then(res => {
+        console.log(res.data.results);
+        dispatch(getTopMoviesSuccess(res.data.results));
+      })
+      .catch(error => console.log(error));
+  };
+};
+
+export const getTopMoviesSuccess = movies => {
   return {
-    type: actionTypes.GET_TOP_MOVIES,
-    movies: topMovies
+    type: actionTypes.GET_TOP_MOVIES_SUCCESS,
+    topMovies: movies
   };
 };
 
