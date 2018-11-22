ec3dfcce17d8f6f62c6de0f1664e2644e76eb097
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-22 10:35:58 +0530

    display movie image and title

--- src/Components/Moviebrowser/Movielist/MovieCard/MovieCard.jsx
+++ src/Components/Moviebrowser/Movielist/MovieCard/MovieCard.jsx
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-
+import Typography from "@material-ui/core/Typography";
 import { CardTitle } from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
-
+import CardContent from "@material-ui/core/CardContent";
 const styles = {
   cardMedia: {
     maxHeight: 394,
@@ -13,8 +13,9 @@ const styles = {
   },
   card: {
     cursor: "pointer",
-    height: 400,
-    overflow: "hidden"
+    height: "auto",
+    overflow: "hidden",
+    margin: "20px"
   },
   bgImage: {
     width: "100%"
@@ -36,7 +37,18 @@ class MovieCard extends Component {
           onMouseOver={() => this.setState({ isMouseOver: true })}
           onMouseLeave={() => this.setState({ isMouseOver: false })}
         >
-          <CardMedia style={styles.cardMedia} image={movie.poster_path} />
+          <CardMedia style={styles.cardMedia}>
+            <img
+              style={styles.bgImage}
+              src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+              alt={movie.title}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {movie.title}
+              </Typography>
+            </CardContent>
+          </CardMedia>
         </Card>
       </div>
     );
