1c4255d0d6766e841a84cfef2335d306643747db
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-02-07 10:24:15 +0530

    final commit

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,8 +1,8 @@
 export const getScrollDownPercentage = () => {
-    const pageHeight = window.document.documentElement.scrollHeight;
-    const clientHeight = window.document.documentElement.clientHeight;
-    const scrollPos = window.pageYOffset;
-    const currentPos = scrollPos + clientHeight;
-    const percentageScrolled = currentPos / pageHeight;
-    return percentageScrolled;
+  const pageHeight = window.document.documentElement.scrollHeight;
+  const clientHeight = window.document.documentElement.clientHeight;
+  const scrollPos = window.pageYOffset;
+  const currentPos = scrollPos + clientHeight;
+  const percentageScrolled = currentPos / pageHeight;
+  return percentageScrolled;
 };
