954d0cf0650d03e035cad44868a1ded1d13a56ea
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-02-07 15:00:23 +0530

    Updates

---
+++ precache-manifest.fc997aa7ca1f3cc558c9fc5c7f5b0412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5727b76424bacc74d6d4",
+    "url": "/moviebrowser/static/js/runtime~main.5727b764.js"
+  },
+  {
+    "revision": "c4cba0fb911cf2cd95a4",
+    "url": "/moviebrowser/static/js/main.c4cba0fb.chunk.js"
+  },
+  {
+    "revision": "695a9ecbbf68d6ef740f",
+    "url": "/moviebrowser/static/js/1.695a9ecb.chunk.js"
+  },
+  {
+    "revision": "c4cba0fb911cf2cd95a4",
+    "url": "/moviebrowser/static/css/main.0d12702a.chunk.css"
+  },
+  {
+    "revision": "1e9e961b3f700444564e89f455434912",
+    "url": "/moviebrowser/index.html"
+  }
+];
